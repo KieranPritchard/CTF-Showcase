121afbabced909d76f845a2fe9edf689f8ee7bb7
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-10 18:14:16 +0000

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import { Routes, Route, NavLink } from 'react-router-dom'
+import Home from './pages/home'
+import WriteUps from './pages/WriteUps'
+import Contact from './pages/Contact'
 
 function App() {
 
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/write-ups" element={<WriteUps />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   )
 }
