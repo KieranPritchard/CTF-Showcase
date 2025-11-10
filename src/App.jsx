a8cb980b309653f9d4b1d48aaa39288d9252da29
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-10 19:42:52 +0000

    added navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Routes, Route, NavLink } from 'react-router-dom'
 import Home from './pages/home'
 import WriteUps from './pages/WriteUps'
 import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/write-ups" element={<WriteUps />} />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar(){
+    const [open, setOpen] = useState(false)
+    return(
+        <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
+            <NavLink>
+                Write-Up Database
+            </NavLink>
+            
+            {/* Desktop menu */}
+            <div className="hidden sm:flex items-center gap-8">
+                <NavLink>Home</NavLink>
+                <NavLink>Write-Ups</NavLink>
+                <NavLink>Contact</NavLink>
+            </div>
+
+            {/* Menu Button */}
+            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="menu" className="sm:hidden">
+                <svg width="21" height="15"  xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 50 50">
+                    <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
+                </svg>
+            </button>
+
+            {/* Mobile menu */}
+            <div className={`${open ? 'flex': 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+                <NavLink className="block py-2 w-full">Home</NavLink>
+                <NavLink className="block py-2 w-full">Write-Ups</NavLink>
+                <NavLink className="block py-2 w-full">Contact</NavLink>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
