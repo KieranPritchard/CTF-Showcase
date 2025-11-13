5c96fba39fafd4036eb217707f1c948bf18af0a4
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-13 22:08:25 +0000

    added icons

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,25 +1,29 @@
 import Background from '../components/Background'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
-function Contact(){
-    return(
+function Contact() {
+    return (
         <>
             <Background isEven={false}>
                 <div className="flex flex-col justify-center items-center h-full">
                     <ul>
                         <li className='text-2xl mb-5 text-[#C7FCEC]'>
-                            <span class="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
-                                k
+                            <span className="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
+                                <FontAwesomeIcon icon={faPhone} />
                             </span>
                             +xx xxxxx xxxxxx
                         </li>
+
                         <li className='text-2xl mb-5 text-[#C7FCEC]'>
-                            <span class="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
-                                k
+                            <span className="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
+                                <FontAwesomeIcon icon={faEnvelope} />
                             </span>
-                            +xx xxxxx xxxxxx
+                            email@example.com
                         </li>
+
                         <li className='text-2xl text-[#C7FCEC]'>
-                            <span class="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
+                            <span className="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
                                 k
                             </span>
                             +xx xxxxx xxxxxx
