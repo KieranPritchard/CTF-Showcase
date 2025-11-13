5392d67944721254b12a34ba1e7c60ba82980b4e
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-13 21:11:25 +0000

    added contact styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,20 +6,20 @@ function Contact(){
             <Background isEven={false}>
                 <div className="flex flex-col justify-center items-center h-full">
                     <ul>
-                        <li className='text-2xl mb-5'>
-                            <span class="bg-gray-300 text-black rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
+                        <li className='text-2xl mb-5 text-[#C7FCEC]'>
+                            <span class="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
                                 k
                             </span>
                             +xx xxxxx xxxxxx
                         </li>
-                        <li className='text-2xl mb-5'>
-                            <span class="bg-gray-300 text-black rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
+                        <li className='text-2xl mb-5 text-[#C7FCEC]'>
+                            <span class="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
                                 k
                             </span>
                             +xx xxxxx xxxxxx
                         </li>
-                        <li className='text-2xl'>
-                            <span class="bg-gray-300 text-black rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
+                        <li className='text-2xl text-[#C7FCEC]'>
+                            <span class="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
                                 k
                             </span>
                             +xx xxxxx xxxxxx
