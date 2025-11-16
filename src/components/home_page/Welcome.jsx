c9e8fe46976b30cfe3eb19cba5f1a0fdc83c2040
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-16 15:42:48 +0000

    fixed some sizing issue

--- src/components/home_page/Welcome.jsx
+++ src/components/home_page/Welcome.jsx
@@ -1,10 +1,10 @@
 function Welcome() {
     return (
-        <div className="bg-[#121A22] border-2 border-[#00FF88] w-full max-w-3xl mx-auto py-10 px-4 sm:px-8 mb-25 rounded-lg shadow-[0_0_20px_#00FF88,0_0_40px_#00FF88,0_0_60px_#00FF88]">
-            <h1 className="headings flicker text-3xl sm:text-4xl md:text-5xl font-extrabold text-center my-4 text-[#C7FCEC] drop-shadow-[0_0_6px_#00FF88] tracking-wide">
+        <div className="bg-[#121A22] border-2 border-[#00FF88] w-full max-w-3xl mx-auto py-4 px-4 sm:px-8 mb-25 rounded-lg shadow-[0_0_20px_#00FF88,0_0_40px_#00FF88,0_0_60px_#00FF88]">
+            <h1 className="headings flicker text-3xl sm:text-3xl md:text-4xl font-extrabold text-center my-4 text-[#C7FCEC] drop-shadow-[0_0_6px_#00FF88] tracking-wide">
                 Explore My Write-Up's
             </h1>
-            <p className="text-base sm:text-lg md:text-xl text-center px-2 sm:px-5 text-[#C7FCEC] leading-relaxed">
+            <p className="text-base sm:text-lg md:text-lg text-center px-2 sm:px-5 text-[#C7FCEC] leading-relaxed">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem earum rerum quisquam similique error quae quaerat laborum repudiandae esse numquam, ipsum consequatur veniam doloribus saepe harum explicabo repellendus tenetur est.
             </p>
         </div>
