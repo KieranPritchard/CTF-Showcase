cefeb3faff8ac16f646324d58dbf88418cefbb28
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-17 18:55:55 +0000

    added fixe

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { Routes, Route, NavLink } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import WriteUps from './pages/WriteUps'
 import Contact from './pages/Contact'
 import WriteUpPage from './pages/WriteUpPage'
--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  base: '/CTF-Showcase/',
   define: {
     global: 'globalThis',
   },
