01fda3f6dc85022af07aa81444896ce2a4289dc2
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-17 19:55:34 +0000

    added proper background

--- src/components/AutoBackground.jsx
+++ src/components/AutoBackground.jsx
@@ -1,6 +1,6 @@
 function AutoBackground({children, isEven}){
     return(
-        <div className={isEven ? "h-auto bg-[#121A22] px-4" : "h-auto bg-[#0A0F14] px-4"}>
+        <div className={isEven ? "h-auto bg-[#121A22] px-4 pb-[3%]" : "h-auto bg-[#0A0F14] px-4 pb-[3%]"}>
             {children}
         </div>
     )
