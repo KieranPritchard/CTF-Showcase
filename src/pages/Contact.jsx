c0905cb369843da5e00aa96966dd285cb1c19bd7
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-17 18:46:49 +0000

    added new icons

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import Background from '../components/Background'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons"
 
 function Contact() {
     return (
@@ -12,21 +13,28 @@ function Contact() {
                             <span className="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
                                 <FontAwesomeIcon icon={faPhone} />
                             </span>
-                            +xx xxxxx xxxxxx
+                            +44 07763 534145
                         </li>
 
                         <li className='text-body text-2xl mb-5 text-[#C7FCEC]'>
                             <span className="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
                                 <FontAwesomeIcon icon={faEnvelope} />
                             </span>
-                            email@example.com
+                            KieranPritchard06@gmail.com
+                        </li>
+
+                        <li className='text-body text-2xl mb-5 text-[#C7FCEC]'>
+                            <span className="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </span>
+                            https://github.com/KieranPritchard
                         </li>
 
                         <li className='text-body text-2xl text-[#C7FCEC]'>
                             <span className="bg-[#00FF88] text-[#0A0F14] rounded-full w-12 h-12 mr-2 inline-flex items-center justify-center">
-                                k
+                                <FontAwesomeIcon icon={faLinkedin} />
                             </span>
-                            +xx xxxxx xxxxxx
+                            https://www.linkedin.com/in/kieran-pritchard/
                         </li>
                     </ul>
                 </div>
