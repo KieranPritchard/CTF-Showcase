c40cf197ccccd6af23fae02ad2406ba2ccf7a51d
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-17 18:59:03 +0000

    added fixes

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/CTF-Showcase">
     <App />
   </BrowserRouter>,
 )
